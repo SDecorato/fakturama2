96167010f902b18ec3604fdd75d5c9caf5b1643c
gerd <gerd@aline-gerd.de>
2010-07-04 20:09:30 +0000

    Added some JavaDoc comments

--- com.sebulli.fakturama/src/com/sebulli/fakturama/editors/TextEditor.java
+++ com.sebulli.fakturama/src/com/sebulli/fakturama/editors/TextEditor.java
@@ -35,65 +35,129 @@ import com.sebulli.fakturama.data.Data;
 import com.sebulli.fakturama.data.DataSetText;
 import com.sebulli.fakturama.views.datasettable.ViewTextTable;
 
+/**
+ * The text editor
+ * 
+ * @author Gerd Bartelt
+ */
 public class TextEditor extends Editor {
+	
+	// Editor's ID
 	public static final String ID = "com.sebulli.fakturama.editors.textEditor";
+
+	// This UniDataSet represents the editor's input 
 	private DataSetText text;
+
+	// SWT widgets of the editor
 	private Text textName;
 	private Text textText;
 	private Text txtCategory;
 
+	// defines, if the text is new created
 	private boolean newText;
 
+	/**
+	 * Constructor
+	 * 
+	 * Associate the table view with the editor
+	 */
 	public TextEditor() {
 		tableViewID = ViewTextTable.ID;
 		editorID = "text";
-
 	}
 
+
+	/**
+	 * Saves the contents of this part
+	 * 
+	 * @param monitor Progress monitor
+	 * @see org.eclipse.ui.part.EditorPart#doSave(org.eclipse.core.runtime.IProgressMonitor)
+	 */
 	@Override
 	public void doSave(IProgressMonitor monitor) {
 		/*
-		 * the following parameters are not saved: - id (constant)
+		 * the following parameters are not saved:
+		 * - id (constant)
 		 */
 
+		// Always set the editor's data set to "undeleted"
 		text.setBooleanValueByKey("deleted", false);
+		
+		// Set the text data
 		text.setStringValueByKey("name", textName.getText());
 		text.setStringValueByKey("text", textText.getText());
 		text.setStringValueByKey("category", txtCategory.getText());
 
+		// If it is a new text, add it to the text list and
+		// to the data base
 		if (newText) {
 			text = Data.INSTANCE.getTexts().addNewDataSet(text);
 			newText = false;
-		} else {
+		}
+		// If it's not new, update at least the data base
+		else {
 			Data.INSTANCE.getTexts().updateDataSet(text);
 		}
 
+		// Refresh the table view of all contacts
 		refreshView();
 	}
 
+	/**
+	 * There is no saveAs function
+	 */
 	@Override
 	public void doSaveAs() {
 	}
 
+	/**
+	 * Initializes the editor. 
+	 * If an existing data set is opened, the local variable "text" is set to
+	 * This data set.
+	 * If the editor is opened to create a new one, a new data set is created and
+	 * the local variable "text" is set to this one.
+	 * 
+	 * @param input The editor's input
+	 * @param site The editor's site
+	 */
 	@Override
 	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+
+		// Set the site and the input
 		setSite(site);
 		setInput(input);
+		
+		// Set the editor's data set to the editor's input
 		text = (DataSetText) ((UniDataSetEditorInput) input).getUniDataSet();
+
+		// test, if the editor is opened to create a new data set. This is,
+		// if there is no input set.
 		newText = (text == null);
 
+		// If new ..
 		if (newText) {
+			
+			// Create a new data set
 			text = new DataSetText(((UniDataSetEditorInput) input).getCategory());
 			setPartName("neuer Text");
 		} else {
+			
+			// Set the Editor's name to the shipping name.
 			setPartName(text.getStringValueByKey("name"));
 		}
 	}
 
+	/**
+	 * Returns whether the contents of this part have changed since the last
+	 * save operation
+	 * 
+	 * @see org.eclipse.ui.part.EditorPart#isDirty()
+	 */
 	@Override
 	public boolean isDirty() {
 		/*
-		 * the following parameters are not checked: - id (constant)
+		 * the following parameters are not checked:
+		 * - id (constant)
 		 */
 
 		if (text.getBooleanValueByKey("deleted")) { return true; }
@@ -105,26 +169,43 @@ public class TextEditor extends Editor {
 		return false;
 	}
 
+	/**
+	 * Returns whether the "Save As" operation is supported by this part.
+
+	 * @see org.eclipse.ui.part.EditorPart#isSaveAsAllowed()
+	 * @return False, SaveAs is not allowed
+	 */
 	@Override
 	public boolean isSaveAsAllowed() {
 		return false;
 	}
 
+	/**
+	* Creates the SWT controls for this workbench part
+	* 
+	* @param the parent control
+	* @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+	*/
 	@Override
 	public void createPartControl(Composite parent) {
 
+		// Create the top Composite
 		Composite top = new Composite(parent, SWT.NONE);
 		GridLayoutFactory.swtDefaults().numColumns(2).applyTo(top);
 
-		Composite invisible = new Composite(top, SWT.NONE);
-		invisible.setVisible(false);
-		GridDataFactory.fillDefaults().hint(0, 0).span(2, 1).applyTo(invisible);
+		// Create an invisible container for all hidden components
+		// There is no invisible component, so no container has to be created
+		//Composite invisible = new Composite(top, SWT.NONE);
+		//invisible.setVisible(false);
+		//GridDataFactory.fillDefaults().hint(0, 0).span(2, 1).applyTo(invisible);
 
+		// Create the title
 		Label labelTitle = new Label(top, SWT.NONE);
 		labelTitle.setText("Text");
 		GridDataFactory.fillDefaults().align(SWT.CENTER, SWT.CENTER).grab(true, false).span(2, 1).applyTo(labelTitle);
 		makeLargeLabel(labelTitle);
 
+		// The name
 		Label labelName = new Label(top, SWT.NONE);
 		labelName.setText("Name");
 		GridDataFactory.swtDefaults().align(SWT.END, SWT.CENTER).applyTo(labelName);
@@ -133,6 +214,7 @@ public class TextEditor extends Editor {
 		superviceControl(textName, 64);
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(textName);
 
+		// The category
 		Label labelCategory = new Label(top, SWT.NONE);
 		labelCategory.setText("Kategorie");
 		GridDataFactory.swtDefaults().align(SWT.END, SWT.CENTER).applyTo(labelCategory);
@@ -141,6 +223,7 @@ public class TextEditor extends Editor {
 		superviceControl(txtCategory, 64);
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(txtCategory);
 
+		// The text
 		Label labelText = new Label(top, SWT.NONE);
 		labelText.setText("Text");
 		GridDataFactory.swtDefaults().align(SWT.END, SWT.CENTER).applyTo(labelText);
