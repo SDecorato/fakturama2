83f4622f03f435488d090c28814db1e5a09e98b3
gerd <gerd@aline-gerd.de>
2010-07-28 18:28:07 +0000

    Moved the webshop_export.php and webshop_export_settings.php into a new folder "webshop"

--- com.sebulli.fakturama/webshop/xtcModified/webshop_export.php
+++ com.sebulli.fakturama/webshop/xtcModified/webshop_export.php
@@ -1,11 +1,30 @@
 <?php
 /*
-  Copyright (c) 2010 Gerd Bartelt
-  Version 1.0.2
-  Date: 2010-07-14
-
-  Released under the GNU General Public License
+ *  Web shop export script
+ *
+ *  Version 1.0.3
+ *  Date: 2010-07-28
+ *
+ *
+ *	Fakturama - Free Invoicing Software 
+ *  Copyright (C) 2010  Gerd Bartelt
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
+ 
+ 
 
 // Use the settings from webshop_export_settings.php, if it exists.
 if( file_exists('webshop_export_settings.php')) {
---
+++ com.sebulli.fakturama/webshop/xtcModified/webshop_export_settings.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ *  User settings included by the export script "webshop_export.php"
+ * 
+ *  Version 1.0.3
+ *  Date: 2010-07-28
+ *
+ *
+ *	Fakturama - Free Invoicing Software 
+ *  Copyright (C) 2010  Gerd Bartelt
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+  
+
+// Define user name and password
+define ('FAKTURAMA_USERNAME',	'user');		
+define ('FAKTURAMA_PASSWORD',	'password');	
+
+// Language code of the product categorie which will be imported.
+// (en = English, de = German, es = Spanish ..) 
+define ('FAKTURAMA_LANGUAGE', 'de');			
+
+// ISO 3166-1-alpha-2 country code of the country of the web shop.
+// (US = USA, GB = United Kingdom, DE = Germany ..) 
+define ('FAKTURAMA_COUNTRY', 'DE');				
+
+?>
