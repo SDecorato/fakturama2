c086bbaa69f52d6e8ab6573672239fa0e65a8619
gerd <gerd@aline-gerd.de>
2010-11-02 13:40:36 +0000

    License: Eclipse Public License - v 1.0

--- com.sebulli.fakturama/src/com/sebulli/fakturama/preferences/PreferencesInDatabase.java
+++ com.sebulli.fakturama/src/com/sebulli/fakturama/preferences/PreferencesInDatabase.java
@@ -1,19 +1,15 @@
-/*
+/* 
+ * Fakturama - Free Invoicing Software - http://fakturama.sebulli.com
  * 
- * Fakturama - Free Invoicing Software Copyright (C) 2010 Gerd Bartelt
+ * Copyright (C) 2010 Gerd Bartelt
  * 
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
+ * Contributors:
+ *     Gerd Bartelt - initial API and implementation
  */
 
 package com.sebulli.fakturama.preferences;
