0e12e7c193e89113534e83e3c1b00964016f27d5
gerd@aline-gerd.de <gerd@aline-gerd.de@4352722c-9db0-447d-9531-ba7cbbd5ab80>
2010-11-14 11:20:25 +0000

    Removed org.eclipse.help.appserver nl_de fragment. Changed the web shop script folder structure.

---
+++ trunk/com.sebulli.fakturama/webshop/oscommerce_based/webshop_export_settings.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * Fakturama - Free Invoicing Software - http://fakturama.sebulli.com
+ * 
+ * Copyright (C) 2010 Gerd Bartelt
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Gerd Bartelt - initial API and implementation
+ */
+  
+
+// Define user name and password
+define ('FAKTURAMA_USERNAME',	'user');		
+define ('FAKTURAMA_PASSWORD',	'password');	
+
+// Language code of the product categorie which will be imported.
+// (en = English, de = German, es = Spanish ..) 
+define ('FAKTURAMA_LANGUAGE', 'de');			
+
+// ISO 3166-1-alpha-2 country code of the country of the web shop.
+// (US = USA, GB = United Kingdom, DE = Germany ..) 
+define ('FAKTURAMA_COUNTRY', 'DE');				
+
+// Character encoding of the confirmation email.
+define ('FAKTURAMA_MAIL_ENCODING', 'ISO-8859-15');				
+
+
+?>
