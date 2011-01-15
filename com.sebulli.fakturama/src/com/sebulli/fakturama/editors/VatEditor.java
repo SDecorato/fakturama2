7ba2c4d8549a51f6d8b9d7328928d7b58fbd8082
gerd <gerd@aline-gerd.de>
2011-01-15 14:27:40 +0000

    Updated the copyright remark to 2011

--- com.sebulli.fakturama/src/com/sebulli/fakturama/editors/VatEditor.java
+++ com.sebulli.fakturama/src/com/sebulli/fakturama/editors/VatEditor.java
@@ -1,7 +1,7 @@
 /* 
  * Fakturama - Free Invoicing Software - http://fakturama.sebulli.com
  * 
- * Copyright (C) 2010 Gerd Bartelt
+ * Copyright (C) 2011 Gerd Bartelt
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
