ce10a10171523342bd46fa1505769b4b37a86140
gerd@aline-gerd.de <gerd@aline-gerd.de@4352722c-9db0-447d-9531-ba7cbbd5ab80>
2011-05-07 16:40:32 +0000

    Set input stream of CSV import to UTF-8 encoding Removed myexporter from core feature.

--- trunk/com.sebulli.fakturama/src/com/sebulli/fakturama/importWizards/CSVImporter.java
+++ trunk/com.sebulli.fakturama/src/com/sebulli/fakturama/importWizards/CSVImporter.java
@@ -17,10 +17,11 @@ package com.sebulli.fakturama.importWizards;
 import static com.sebulli.fakturama.Translate._;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 import com.sebulli.fakturama.ApplicationWorkbenchAdvisor;
@@ -31,6 +32,7 @@ import com.sebulli.fakturama.data.DataSetExpenditure;
 import com.sebulli.fakturama.data.DataSetExpenditureItem;
 import com.sebulli.fakturama.data.DataSetVAT;
 import com.sebulli.fakturama.editors.ExpenditureEditor;
+import com.sebulli.fakturama.logger.Logger;
 import com.sebulli.fakturama.views.datasettable.ViewExpenditureTable;
 import com.sebulli.fakturama.views.datasettable.ViewListTable;
 
@@ -119,183 +121,187 @@ public class CSVImporter {
 		// Count the line of the import file
 		int lineNr = 0;
 
-		// Create a File object
-		File file = new File(fileName);
 
-		// If the log file exists read the content
-		if (file.exists()) {
+		// Open the existing file
+		InputStreamReader isr;
+		BufferedReader in = null;
+	
+		try {
+			isr = new InputStreamReader(new FileInputStream(fileName),"UTF-8");
+			in = new BufferedReader(isr);
+		}
+		catch (UnsupportedEncodingException e) {
+			Logger.logError(e, "Unsupported UTF-8 encoding");
+			result += NL + "Unsupported UTF-8 encoding";
+			return;
+		}
+		catch (FileNotFoundException e) {
+			//T: Error message
+			result += NL + _("File not found.");
+			return;
+		}
 
-			// Open the existing file
-			BufferedReader in;
-			try {
-				in = new BufferedReader(new FileReader(file));
-			}
-			catch (FileNotFoundException e1) {
-				//T: Error message
-				result += NL + _("File not found.");
-				return;
-			}
+		String line = "";
+		String[] columns;
 
-			String line = "";
-			String[] columns;
+		// Read the first line
+		try {
+			if ((line = in.readLine()) != null) {
+				lineNr++;
 
-			// Read the first line
-			try {
-				if ((line = in.readLine()) != null) {
-					lineNr++;
-
-					// Get the headers of the columns
-					columns = line.split(";");
-					for (int i = 0; i < columns.length; i++) {
-						columns[i] = removeQuotes(columns[i]);
-					}
-
+				// Get the headers of the columns
+				columns = line.split(";");
+				for (int i = 0; i < columns.length; i++) {
+					columns[i] = removeQuotes(columns[i]);
 				}
-				else {
-					return;
-				}
+
 			}
-			catch (IOException e1) {
+			else {
 				//T: Error message
 				result += NL + _("Error reading the first line");
 				return;
 			}
+		}
+		catch (IOException e1) {
+			//T: Error message
+			result += NL + _("Error reading the first line");
+			return;
+		}
 
-			// Read the existing file and store it in a buffer
-			// with a fix size. Only the newest lines are kept.
-			try {
+		// Read the existing file and store it in a buffer
+		// with a fix size. Only the newest lines are kept.
+		try {
 
-				// Store the last expenditure. This is used to import
-				// 2 lines with 2 expenditure items but only one expenditure.
-				DataSetExpenditure lastExpenditure = null;
+			// Store the last expenditure. This is used to import
+			// 2 lines with 2 expenditure items but only one expenditure.
+			DataSetExpenditure lastExpenditure = null;
 
-				// Read line by line
-				while ((line = in.readLine()) != null) {
-					lineNr++;
+			// Read line by line
+			while ((line = in.readLine()) != null) {
+				lineNr++;
 
-					DataSetExpenditure expenditure = new DataSetExpenditure();
-					DataSetExpenditureItem expenditureItem = new DataSetExpenditureItem();
-					Properties prop = new Properties();
+				DataSetExpenditure expenditure = new DataSetExpenditure();
+				DataSetExpenditureItem expenditureItem = new DataSetExpenditureItem();
+				Properties prop = new Properties();
 
-					// Get the cells
-					String[] cells = line.split(";");
+				// Get the cells
+				String[] cells = line.split(";");
 
-					// Dispatch all the cells into a property
-					for (int col = 0; col < cells.length; col++) {
-						if (col < columns.length) {
+				// Dispatch all the cells into a property
+				for (int col = 0; col < cells.length; col++) {
+					if (col < columns.length) {
 
-							if (isRequiredColumn(columns[col])) {
-								prop.setProperty(columns[col].toLowerCase(), removeQuotes(cells[col]));
-							}
+						if (isRequiredColumn(columns[col])) {
+							prop.setProperty(columns[col].toLowerCase(), removeQuotes(cells[col]));
 						}
 					}
+				}
 
-					// Test, if all columns are used
-					if ((prop.size() > 0) && (prop.size() != requiredHeaders.length)) {
-						for (int i = 0; i < requiredHeaders.length; i++) {
-							if (!prop.containsKey(requiredHeaders[i]))
-								//T: Format: LINE: xx: NO DATA IN COLUMN yy FOUND.
-								result += NL + _("Line") + ": " + Integer.toString(lineNr) + ": " + 
-								//T: Format: LINE: xx: NO DATA IN COLUMN yy FOUND.
-								_("No Data in Column") + " \"" + requiredHeaders[i] + "\" " + 
-								//T: Format: LINE: xx: NO DATA IN COLUMN yy FOUND.
-								_("found.");
-						}
+				// Test, if all columns are used
+				if ((prop.size() > 0) && (prop.size() != requiredHeaders.length)) {
+					for (int i = 0; i < requiredHeaders.length; i++) {
+						if (!prop.containsKey(requiredHeaders[i]))
+							//T: Format: LINE: xx: NO DATA IN COLUMN yy FOUND.
+							result += NL + _("Line") + ": " + Integer.toString(lineNr) + ": " + 
+							//T: Format: LINE: xx: NO DATA IN COLUMN yy FOUND.
+							_("No Data in Column") + " \"" + requiredHeaders[i] + "\" " + 
+							//T: Format: LINE: xx: NO DATA IN COLUMN yy FOUND.
+							_("found.");
 					}
-					else {
+				}
+				else {
 
-						// Date is a must.
-						if (!prop.getProperty("date").isEmpty()) {
+					// Date is a must.
+					if (!prop.getProperty("date").isEmpty()) {
 
-							// Fill the expenditure data set
-							expenditure.setStringValueByKey("name", prop.getProperty("name"));
-							expenditure.setStringValueByKey("category", prop.getProperty("category"));
-							expenditure.setStringValueByKey("date", DataUtils.DateAsISO8601String(prop.getProperty("date")));
-							expenditure.setStringValueByKey("nr", prop.getProperty("nr"));
-							expenditure.setStringValueByKey("documentnr", prop.getProperty("documentnr"));
+						// Fill the expenditure data set
+						expenditure.setStringValueByKey("name", prop.getProperty("name"));
+						expenditure.setStringValueByKey("category", prop.getProperty("category"));
+						expenditure.setStringValueByKey("date", DataUtils.DateAsISO8601String(prop.getProperty("date")));
+						expenditure.setStringValueByKey("nr", prop.getProperty("nr"));
+						expenditure.setStringValueByKey("documentnr", prop.getProperty("documentnr"));
 
-							// Test, if the last line was the same expenditure
-							boolean repeatedExpenditure = false;
+						// Test, if the last line was the same expenditure
+						boolean repeatedExpenditure = false;
 
-							if (lastExpenditure != null)
-								if (lastExpenditure.isTheSameAs(expenditure))
-									repeatedExpenditure = true;
+						if (lastExpenditure != null)
+							if (lastExpenditure.isTheSameAs(expenditure))
+								repeatedExpenditure = true;
 
-							// If the data set is already existing, stop the CSV import
-							if (!repeatedExpenditure)
-								if (!Data.INSTANCE.getExpenditures().isNew(expenditure)) {
-									//T: Error message Dataset is already imported
-									result += NL + _("Dataset is already imported");
-									//T: Error message: DATASET xx FROM date
-									result += NL + _("Dataset")+ " " + prop.getProperty("name") + " " +
-										//T: DATASET xx FROM date
-										_("from", "DATE") + " " + prop.getProperty("date");
-									//T: Error message Import stopped
-									result += NL + _("Import stopped");
-									break;
-								}
-
-							// Fill the expenditure item with data
-							expenditureItem.setStringValueByKey("name", prop.getProperty("item name"));
-							expenditureItem.setStringValueByKey("category", prop.getProperty("item category"));
-							expenditureItem.setStringValueByKey("price", prop.getProperty("item price"));
-
-							String vatName = prop.getProperty("item vat");
-
-							Double vatValue = DataUtils.StringToDouble(prop.getProperty("vat"));
-							DataSetVAT vat = new DataSetVAT(vatName, DataSetVAT.getPurchaseTaxString(), vatName, vatValue);
-							vat = Data.INSTANCE.getVATs().addNewDataSetIfNew(vat);
-							expenditureItem.setIntValueByKey("vatid", vat.getIntValueByKey("id"));
-
-							// Add the expenditure and expenditure item to the data base
-							expenditureItem = Data.INSTANCE.getExpenditureItems().addNewDataSet(expenditureItem);
-							ExpenditureEditor.updateBillingAccount (expenditureItem);
-							expenditure = Data.INSTANCE.getExpenditures().addNewDataSetIfNew(expenditure);
-
-							// Add the item to the item string
-							String oldItems = expenditure.getStringValueByKey("items");
-							String newItem = expenditureItem.getStringValueByKey("id");
-							if (!oldItems.isEmpty())
-								oldItems += ",";
-							else {
-								importedExpenditures++;
+						// If the data set is already existing, stop the CSV import
+						if (!repeatedExpenditure)
+							if (!Data.INSTANCE.getExpenditures().isNew(expenditure)) {
+								//T: Error message Dataset is already imported
+								result += NL + _("Dataset is already imported");
+								//T: Error message: DATASET xx FROM date
+								result += NL + _("Dataset")+ " " + prop.getProperty("name") + " " +
+									//T: DATASET xx FROM date
+									_("from", "DATE") + " " + prop.getProperty("date");
+								//T: Error message Import stopped
+								result += NL + _("Import stopped");
+								break;
 							}
-							expenditure.setStringValueByKey("items", oldItems + newItem);
 
-							// Recalculate the total sum of all items and set the total value
-							expenditure.calculate();
-							// Get the total result
-							Double total = expenditure.getSummary().getTotalGross().asDouble();
+						// Fill the expenditure item with data
+						expenditureItem.setStringValueByKey("name", prop.getProperty("item name"));
+						expenditureItem.setStringValueByKey("category", prop.getProperty("item category"));
+						expenditureItem.setStringValueByKey("price", prop.getProperty("item price"));
 
-							System.out.println(total);
-							// Update the text widget
-							expenditure.setDoubleValueByKey("total", total);
-							expenditure.setDoubleValueByKey("paid", total);
-						
+						String vatName = prop.getProperty("item vat");
 
-							Data.INSTANCE.getExpenditures().updateDataSet(expenditure);
+						Double vatValue = DataUtils.StringToDouble(prop.getProperty("vat"));
+						DataSetVAT vat = new DataSetVAT(vatName, DataSetVAT.getPurchaseTaxString(), vatName, vatValue);
+						vat = Data.INSTANCE.getVATs().addNewDataSetIfNew(vat);
+						expenditureItem.setIntValueByKey("vatid", vat.getIntValueByKey("id"));
 
-							// Set the reference of the last expenditure to this one
-							lastExpenditure = expenditure;
+						// Add the expenditure and expenditure item to the data base
+						expenditureItem = Data.INSTANCE.getExpenditureItems().addNewDataSet(expenditureItem);
+						ExpenditureEditor.updateBillingAccount (expenditureItem);
+						expenditure = Data.INSTANCE.getExpenditures().addNewDataSetIfNew(expenditure);
+
+						// Add the item to the item string
+						String oldItems = expenditure.getStringValueByKey("items");
+						String newItem = expenditureItem.getStringValueByKey("id");
+						if (!oldItems.isEmpty())
+							oldItems += ",";
+						else {
+							importedExpenditures++;
 						}
+						expenditure.setStringValueByKey("items", oldItems + newItem);
 
+						// Recalculate the total sum of all items and set the total value
+						expenditure.calculate();
+						// Get the total result
+						Double total = expenditure.getSummary().getTotalGross().asDouble();
+
+						System.out.println(total);
+						// Update the text widget
+						expenditure.setDoubleValueByKey("total", total);
+						expenditure.setDoubleValueByKey("paid", total);
+					
+
+						Data.INSTANCE.getExpenditures().updateDataSet(expenditure);
+
+						// Set the reference of the last expenditure to this one
+						lastExpenditure = expenditure;
 					}
 
 				}
-				
-				// Refresh the views
-				ApplicationWorkbenchAdvisor.refreshView(ViewListTable.ID);
-				ApplicationWorkbenchAdvisor.refreshView(ViewExpenditureTable.ID);
-				
-				// The result string
-				//T: Message: xx VOUCHERS HAVE BEEN IMPORTED 
-				result += NL + Integer.toString(importedExpenditures) + " " + _("Vouchers have been imported.");
 
 			}
-			catch (IOException e) {
-				//T: Error message
-				result += NL + _("Error opening the file.");
-			}
+			
+			// Refresh the views
+			ApplicationWorkbenchAdvisor.refreshView(ViewListTable.ID);
+			ApplicationWorkbenchAdvisor.refreshView(ViewExpenditureTable.ID);
+			
+			// The result string
+			//T: Message: xx VOUCHERS HAVE BEEN IMPORTED 
+			result += NL + Integer.toString(importedExpenditures) + " " + _("Vouchers have been imported.");
+
+		}
+		catch (IOException e) {
+			//T: Error message
+			result += NL + _("Error opening the file.");
 		}
 
 	}
