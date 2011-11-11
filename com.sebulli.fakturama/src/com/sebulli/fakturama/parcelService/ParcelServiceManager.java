5a8b99e4cdd3ab249b18eeea044c596da433209e
gerd <gerd@aline-gerd.de>
2011-11-11 18:06:03 +0000

    Added comments for ParcelServiceManager Class

--- com.sebulli.fakturama/src/com/sebulli/fakturama/parcelService/ParcelServiceManager.java
+++ com.sebulli.fakturama/src/com/sebulli/fakturama/parcelService/ParcelServiceManager.java
@@ -23,30 +23,58 @@ import java.util.Properties;
 
 import com.sebulli.fakturama.TemplateFilename;
 
+/**
+ * Loads the parcel service configurations files into properties lists.  
+ * 
+ * @author Gerd Bartelt
+ *
+ */
 public class ParcelServiceManager {
 	
+	// All properties lists of all parcel services
 	private ArrayList<Properties> propertiesList;
+	
+	// The active parcel service
 	private int active = -1;
 	
+	/**
+	 * Loads all the parcel service lists from a specified path
+	 * 
+	 * @param templatePath
+	 * 		The path where all the files are listed
+	 */
 	public ParcelServiceManager (String templatePath) {
 		
+		// Clear all, and create a new array list
 		propertiesList = new ArrayList<Properties>();
 		
+		// Get the directory and find all files
 		File dir = new File(templatePath);
 		String[] children = dir.list();
+
+		// Get all files
 		if (children != null) {
 			for (int i = 0; i < children.length; i++) {
+				
 				// Get filename of file or directory
 				TemplateFilename templateFilename = new TemplateFilename(templatePath, children[i]);
+				
+				// It's used as a parcel service file, if it ends with a *.txt
 				if (templateFilename.getExtension().equalsIgnoreCase(".txt")) {
+
+					// Load the file into a properties object
 					Properties properties = new Properties();
 					BufferedInputStream stream;
 					try {
 						stream = new BufferedInputStream(new FileInputStream(templateFilename.getPathAndFilename()));
 						properties.load(stream);
 						stream.close();
+
+						// Use it only, if there is at least a name and a url key
 						if (properties.containsKey("name") && properties.containsKey("url")) {
 							propertiesList.add(properties);
+							
+							// Select this as active
 							active = propertiesList.size()-1;
 						}
 					} catch (IOException e) {
@@ -57,31 +85,77 @@ public class ParcelServiceManager {
 		}
 	}
 
+	/**
+	 * Getter for the number of properties entries
+	 * 
+	 * @return
+	 * 	The number of properties entries
+	 */
 	public int size() {
 		return propertiesList.size();
 	}
 	
+	/**
+	 * Get the name of the parcel service
+	 * 
+	 * @param i
+	 * 		The number of the properties object
+	 * @return
+	 * 		The name of the parcel service
+	 */
 	public String getName(int i) {
 		return propertiesList.get(i).getProperty("name");
 	}
 
+	/**
+	 * Get the name of the active parcel service
+	 * 
+	 * @return
+	 * 		The name of the active parcel service
+	 */
 	public String getName() {
 		return propertiesList.get(active).getProperty("name");
 	}
 
+	/**
+	 * Get the URL of the parcel service
+	 * 
+	 * @param i
+	 * 		The number of the properties object
+	 * @return
+	 * 		The URL of the parcel service
+	 */
 	public String getUrl(int i) {
 		return propertiesList.get(i).getProperty("url");
 	}
 
+	/**
+	 * Get the URL of the active parcel service
+	 * 
+	 * @return
+	 * 		The URL of the active parcel service
+	 */
 	public String getUrl() {
 		return propertiesList.get(active).getProperty("url");
 	}
 	
+	/**
+	 * Set the active properties object
+	 * 
+	 * @param i
+	 * 		Number of the properties object
+	 */
 	public void setActive (int i) {
 		if ( (i< propertiesList.size()) && (i>=0))
 			active = i;
 	}
 	
+	/**
+	 * Get the active properties object
+	 * 
+	 * @return
+	 * 		The active properties object
+	 */
 	public Properties getProperties () {
 		return propertiesList.get(active);
 	}
